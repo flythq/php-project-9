aac91ad2aad3c3a3371a965b27387cad742378fb
Andrey Nikolaenkov <nichexplore@gmail.com>
2026-03-16 19:31:34 +0300

    Add main page

--- public/index.php
+++ public/index.php
@@ -5,27 +5,30 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
 
-$container->set('renderer', function () {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../templates');
 });
+
+$app = AppFactory::createFromContainer($container);
+
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
 $container->set('flash', function () {
     return new \Slim\Flash\Messages();
 });
 
-$app = AppFactory::createFromContainer($container);
-
-$app->addErrorMiddleware(true, true, true);
-$app->add(MethodOverrideMiddleware::class);
-
-$router = $app->getRouteCollector()->getRouteParser();
-
 $app->get('/', function ($request, $response) {
-
-    return $response->write('Привет, мир!');
+    return $this->get(Twig::class)->render($response, 'layout.html.twig');
 });
 
 $app->run();
